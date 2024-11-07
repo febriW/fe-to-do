b9850a4e8a90b09a09af64a13cf85275267235c5
febriw <febriwanda26@gmail.com>
2024-11-08 02:05:43 +0700

    added layouts and pages dashboard

---
+++ layouts/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col gap-10 h-screen w-full">
+        <nav class="bg-gray-300 text-center py-4">
+            Simple To do apps
+        </nav>
+        <div class="flex justify-center">
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ pages/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-md">
+        here
+    </div>
+</template>
+<script setup lang="ts">
+    definePageMeta({
+    layout: 'dashboard'
+    })
+</script>
