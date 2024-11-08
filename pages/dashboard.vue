79cec10815dda53710a64274d800058aa9a630e9
febriw <febriwanda26@gmail.com>
2024-11-08 14:24:11 +0700

    update login page and dashboard, setup middleware

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -5,6 +5,7 @@
 </template>
 <script setup lang="ts">
     definePageMeta({
-    layout: 'dashboard'
+        middleware: 'auth',
+        layout: 'dashboard'
     })
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,75 @@
 <template>
-    <div class="w-full">
+    <div class="">
         <n-card title="Login Form">
-            Here will i create login form 
+            <div class="flex flex-col gap-4">
+                <n-input 
+                    size="large" 
+                    placeholder="Masukan email" 
+                    v-model:value="email"
+                />
+                <n-input
+                    size="large"
+                    type="password"
+                    show-password-on="mousedown"
+                    placeholder="Password"
+                    v-model:value="password"
+                />
+                <div class="mt-6 flex flex-row gap-4 justify-end">
+                    <n-button type="primary" @click.prevent="processLogin">Login</n-button>
+                    <n-button type="info" @click="navigateTo('/register')">Daftar</n-button>
+                </div>
+            </div>
         </n-card>
+        <n-modal v-model:show="showModal">
+            <n-card
+                style="width: 600px"
+                title="Modal"
+                :bordered="false"
+                size="huge"
+                role="dialog"
+                aria-modal="true"
+            >
+            <template #header-extra>
+                Oops Something Wrong!
+            </template>
+            {{msgError}}
+            </n-card>
+        </n-modal>
     </div>
 </template>
 <script lang="ts" setup>
-import {
-    NCard
-} from 'naive-ui'
+    import { storeToRefs } from 'pinia';
+    import { useAuthStore } from '~/store/auth';
+    import {
+        NCard,
+        NInput,
+        NButton,
+        NModal
+    } from 'naive-ui'
+
+    definePageMeta({
+        middleware: 'auth'
+    })
+
+    const { login } = useAuthStore()
+    const { authenticated, msgError } = storeToRefs(useAuthStore())
+
+   let email = ref("")
+   let password = ref("")
+   let showModal = ref(false)
+
+    const processLogin = async () => {
+        await login({
+            email: email.value,
+            password: password.value
+        })
+        if(authenticated) { navigateTo('/dashboard') }
+        if(msgError.value !== '') { showModal.value = true }
+    }
+
 </script>
 <style scoped>
     .n-card {
-        max-width: 700px;
+        width: 500px;
     }
 </style>
